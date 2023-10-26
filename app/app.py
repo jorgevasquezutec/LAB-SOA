d104728349d64e5a0702995584c7abe1d95b1c1b
jorgevasquezutec <jorge.vasquez@utec.edu.pe>
2023-10-25 21:23:24 -0500

    feat: save all

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request,Header, Response
 from starlette.middleware.cors import CORSMiddleware
 from app.config.settings import api_settings
 import uvicorn
@@ -7,8 +7,9 @@ from fastapi.responses import JSONResponse
 from datetime import datetime
 from app.constants.values import LOG_API_PATHS
 import requests
-import concurrent.futures
-
+# import concurrent.futures
+import app.services as services
+import xml.etree.ElementTree as ET
 
 app = FastAPI(
     title=api_settings.TITLE,
@@ -29,38 +30,53 @@ app.add_middleware(
 app.router.prefix = api_settings.PREFIX
 
 
+def get_response(data, response_format):
+    if response_format == "text/xml" or response_format == "application/xml":
+        root = ET.Element("data")
+        for restaurant in data:
+            item = ET.SubElement(root, "restaurant")
+            for key, value in restaurant.items():
+                elem = ET.SubElement(item, key)
+                elem.text = str(value)
+        response = ET.tostring(root, encoding="unicode")
+        return Response(content=response, media_type="application/xml")
+    else:
+        return JSONResponse(content={"data": data})
+
+
 @app.get("/")
 def root():
     return {"message": f"Welcome to {api_settings.TITLE}"}
 
+@app.get('/ciudad/{ciudad}/restaurante')
+def get_restaurantes(ciudad: str, Accept: str = Header(None)):
+    restaurants = services.get_restaurants(ciudad)
+        # print(restaurants)
+    if(len(restaurants) == 0):
+        return JSONResponse(content={
+            "message": "No se encontraron restaurantes en la ciudad"
+        }, status_code=404)
+    return get_response(restaurants, Accept)
 
-def fetch_data(api_url):
-    response = requests.get(api_url)
-    return response.json()
+@app.get('/ciudad/{ciudad}/clima/manhana')
+def get_clima_manhana(ciudad: str, Accept: str = Header(None)):
+    clima = services.getHourlyWeather(ciudad)
+    # print(restaurants)
+    if(len(clima) == 0):
+        return JSONResponse(content={
+            "message": "No se encontraron restaurantes en la ciudad"
+        }, status_code=404)
+    return get_response(clima, Accept)
 
-
-# @app.get("/search/{name}")
-# # @log_request()
-# def root(name: str):
-#     try:
-#         name = name.lower()
-
-#         api_urls = [
-#             f'{api_settings.API_GATEWAY}poke_api/search/{name}',
-#             f'{api_settings.API_GATEWAY}poke_stats/search/{name}',
-#             f'{api_settings.API_GATEWAY}poke_images/search/{name}',
-#         ]
-#         # print(api_urls)
-
-#         with concurrent.futures.ThreadPoolExecutor() as executor:
-#             results = list(executor.map(fetch_data, api_urls))
-
-#         return {"data": results}
-
-#     except Exception as e:
-#         print(e)
-#         raise HTTPException(status_code=500, detail=str(e))
-
+@app.get('/ciudad/{ciudad}/clima/7dias')
+def get_clima_7dias(ciudad: str, Accept: str = Header(None)):
+    clima = services.getDailyWeather(ciudad)
+    # print(restaurants)
+    if(len(clima) == 0):
+        return JSONResponse(content={
+            "message": "No se encontraron restaurantes en la ciudad"
+        }, status_code=404)
+    return get_response(clima, Accept)
 
 def run():
     uvicorn.run(app,
---
+++ app/services.py
@@ -0,0 +1,109 @@
+
+import requests
+import xml.etree.ElementTree as ET
+from fastapi.exceptions import HTTPException
+
+
+DELTA = 0.01
+
+def fetch_data(api_url):
+    response = requests.get(api_url)
+    return response.json()
+
+def fetch_data_xml(api_url):
+    response = requests.get(api_url)
+    root = ET.fromstring(response.content)
+    return root
+
+def create_bounding_box(latitude, longitude, delta):
+      # Calculate the latitude and longitude for the upper-left corner of the bounding box
+    upper_left_lat = round(latitude - delta, 7)
+    upper_left_lon = round(longitude - delta, 7)
+
+    # Calculate the latitude and longitude for the lower-right corner of the bounding box
+    lower_right_lat = round(latitude + delta, 7)
+    lower_right_lon = round(longitude + delta, 7)
+    return (upper_left_lat, upper_left_lon, lower_right_lat, lower_right_lon)
+
+
+def getLatitudLongitud(ciudad):
+    ciudad = ciudad.lower()
+    URL = 'https://nominatim.openstreetmap.org/search?q='+ciudad+'&format=json'
+    data = fetch_data(URL)
+    print(len(data))
+    if(len(data) == 0):
+        print("no se encontro")
+        raise HTTPException(status_code=404,detail="No se encontro la ciudad")
+    print("paso")
+    lat = data[0]['lat']
+    lat = float(lat)
+    lon = data[0]['lon']
+    lon = float(lon)
+    return lat, lon
+
+
+def getResutaranteBYBox(box):
+    URL = 'https://api.openstreetmap.org/api/0.6/map?bbox='+str(box[1])+','+str(box[0])+','+str(box[3])+','+str(box[2])
+    data = fetch_data_xml(URL)
+    return data
+
+def getXMLRestaurantes(res):
+    rdata = []
+    for element in res:
+        # print(element)
+        restaurant_info = {}
+        # Iterar a través de los tags del elemento
+        for tag in element:
+            if 'k' in tag.attrib and 'v' in tag.attrib:
+                k = tag.attrib['k']
+                v = tag.attrib['v']
+                restaurant_info[k] = v
+        
+        # Verificar si el elemento representa un restaurante
+        if 'amenity' in restaurant_info and restaurant_info['amenity'] == 'restaurant':
+            rdata.append(restaurant_info)
+    # print(rdata)
+    return rdata
+
+
+def get_restaurants(ciudad: str):
+    lat,long = getLatitudLongitud(ciudad)
+    # print(lat,long)
+    box = create_bounding_box(lat,long,DELTA)
+    # print(box)
+    data = getResutaranteBYBox(box)
+    # print(data)
+    restaurants = getXMLRestaurantes(data)
+    return restaurants
+
+def getDailyWeather(ciudad: str):
+    lat,long = getLatitudLongitud(ciudad)
+    #https://api.open-meteo.com/v1/forecast?latitude=-12.04&longitude=-77.03&forecast_days=2&daily=temperature_2m_max&timezone=PST
+    URL = 'https://api.open-meteo.com/v1/forecast?latitude='+str(lat)+'&longitude='+str(long)+'&forecast_days=7&daily=temperature_2m_max&timezone=PST'
+    data = fetch_data(URL) #temperature_2m_max
+    # pair up (time,temperature_2m_max)
+    result = []
+    for i in range(len(data['daily']['temperature_2m_max'])):
+        obj = {}
+        key = data['daily']['time'][i]
+        obj[key] = data['daily']['temperature_2m_max'][i]
+        result.append(obj)
+        
+    return result
+
+def getHourlyWeather(ciudad: str):
+    lat,long = getLatitudLongitud(ciudad)
+    # print(lat,long)
+    #https://api.open-meteo.com/v1/forecast?latitude=-12.04&longitude=-77.03&forecast_days=2&hourly=temperature_2m&timezone=PST
+    URL = 'https://api.open-meteo.com/v1/forecast?latitude='+str(lat)+'&longitude='+str(long)+'&forecast_days=2&hourly=temperature_2m&timezone=PST'
+    print(URL)
+    data = fetch_data(URL)#temperature_2m
+    print(data)
+    # pair up (time,temperature_2m)
+    result = []
+    for i in range(len(data['hourly']['temperature_2m'])):
+        obj = {}
+        key = data['hourly']['time'][i]
+        obj[key] = data['hourly']['temperature_2m'][i]
+        result.append(obj)
+    return result
